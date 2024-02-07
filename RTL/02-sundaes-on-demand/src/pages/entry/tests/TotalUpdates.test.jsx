b790a8fae603c7c8f71e09f154c7573072b4ab73
rlorxl <gooodgirl08@gmail.com>
2024-02-07 16:55:12 +0900

    TotalUpdates 테스트

---
+++ RTL/02-sundaes-on-demand/src/pages/entry/tests/TotalUpdates.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import Options from '../Options';
+import { expect } from 'vitest';
+import { userEvent } from '@testing-library/user-event';
+
+test.skip('update scoop subtotal when scoops change', async () => {
+  const user = userEvent.setup();
+  render(<Options optionType='scoops' />);
+
+  // 총금액 합계가 $0.00에서 시작하는지 확인
+  const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false }); // * exact: 전체문자열이 일치하는지 확인한다. (false: 부분만 일치)
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+  // vanilla scoop 1추가 후, subtotal 확인
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  }); // * 비동기 업데이트 요소에 find쿼리 사용.
+
+  await user.clear(vanillaInput); // * 다음단계로 넘어가기전 이미 입력되어있던 내용에 대한 clear 수행.
+  await user.type(vanillaInput, '1');
+  expect(scoopsSubtotal).toHaveTextContent('2.00');
+
+  // chocolate scoop 2추가 후, subtotal 확인
+  const chocolateInput = await screen.findByRole('spanbutton', {
+    name: 'Chocolate',
+  });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, '2');
+  expect(scoopsSubtotal).toHaveTextContent('6.00');
+});
